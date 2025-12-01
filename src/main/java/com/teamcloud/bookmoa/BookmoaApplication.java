b0d0f4481d314f79819d23fe46463ded608aba90
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-12-01 20:35:57 +0900

    초기 커밋

---
+++ src/main/java/com/teamcloud/bookmoa/BookmoaApplication.java
@@ -0,0 +1,13 @@
+package com.teamcloud.bookmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmoaApplication.class, args);
+	}
+
+}
